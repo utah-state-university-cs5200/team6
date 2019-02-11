4c9cef0424cde8c9b0a77482f665ae8c2ac31e36
EvanBurke <evan.burke@aggiemail.usu.edu>
2019-02-11 13:58:58 -0700

    Added the beginnings of a Message class library.

---
+++ MessageTypes/MessageTypes/AckMessage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MessageTypes
+{
+    public class AckMessage : Message
+    {
+        public short ID;
+        public bool gORp;
+
+        //
+        public AckMessage(short ID, bool gORp)
+        {
+            MessageType = 5;
+            this.ID = ID;
+            this.gORp = gORp;
+        }
+
+        public override void DoMessageTask()
+        {
+            //Do Stuff
+        }
+
+        public override Byte[] Encode()
+        {
+            //TODO:
+            //Encode MessageType
+            //Encode ID
+            //Encode gORp
+            //Return byte[] message
+            return null;
+        }
+
+        //For use in the Message Factory
+        public static Message Decode(MemoryStream f, Byte[] items)
+        {
+            //TODO:
+            //Read byte MessageType
+            //Read short ID
+            //Read byte gORp
+            //return new Message(MessageType, ID, gORp)
+            return null;
+        }
+    }
+}
---
+++ MessageTypes/MessageTypes/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MessageTypes
+{
+    public abstract class Message
+    {
+        public short MessageType;
+
+        // As it stands, what DoMessageTask will need to take in as a parameter is undetermined
+        public abstract void DoMessageTask();
+
+        // Each message type will have its own encoding based on protocol
+        public abstract Byte[] Encode();
+
+        // Cannot make an abstract static function
+        // will put the static Decode in each individual message anyways
+    }
+}
